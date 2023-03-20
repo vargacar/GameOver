3790a3063ad8a065ea5e47cf62b5c7f6e741e501
vargacar <c_vargas88@yahoo.es>
2023-03-19 22:55:50 -0400

    updating code

--- Game.js
+++ Game.js
@@ -6,77 +6,77 @@ const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
 let acceptingAnswers = true
-let score = 0 
-let questionCounter = 0 
-let availableQuestion = []
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
 let questions = [
     {
-        question: 'Baby camels are born with..?',
-        choice1: '3 humps',
-        choice2: 'no hump',
-        choice3: '1 hump',
-        choice4: '2 humps',
-        answer: 2,
-    },
-    {
-        question: 'How many sides does an octacontagon have?',
-        choice1: '8',
-        choice2: '18',
-        choice3: '80',
-        choice4: '88',
+        question: 'What is 7 * 3?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
         answer: 3,
     },
     {
-        question: 'How many are in a dozen bakery?',
-        choice1: '12',
-        choice2: '10',
-        choice3: '22',
-        choice4: '13',
+        question:
+            "How many are in a dozen bakery?",
+        choice1: "12",
+        choice2: "10",
+        choice3: "22",
+        choice4: "13",
         answer: 4,
     },
     {
-        question: 'A googol is the number 1 with..?',
-        choice1: '20 zeros after it',
-        choice2: '1 zero after it',
-        choice3: '5 zeros after it',
-        choice4: '100 zeroes after it',
-        answer: 4,
+        question: "How many sides does an octacontagon have?",
+        choice1: "8",
+        choice2: "18",
+        choice3: "80",
+        choice4: "88",
+        answer: 3,
+    },
+    {
+        question: "Baby camels are born with how many humps?",
+        choice1: "3",
+        choice2: "0",
+        choice3: "5",
+        choice4: "2",
+        answer: 2,
     }
-
 ]
 
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4
 
 startGame = () => {
-    questionCounter = 0 
+    questionCounter = 0
     score = 0
-    availableQuestion = [...questions]
+    availableQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = () => {
-    if(availableQuestion.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('highscores.html')
     }
 
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
     progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-
-    const questionsIndex = Math.floor(Math.random() * availableQuestion.lenght)
-    currentQuestion = availableQuestion[questionsIndex]
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice + number']
+        choice.innerText = currentQuestion['choice' + number]
     })
 
-    availableQuestion.splice(questionsIndex, 1)
+    availableQuestions.splice(questionsIndex, 1)
 
     acceptingAnswers = true
 }
@@ -89,8 +89,7 @@ choices.forEach(choice => {
         const selectedChoice = e.target
         const selectedAnswer = selectedChoice.dataset['number']
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
-        'incorrect'
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if(classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
